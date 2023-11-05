f90b89da840da4a9330e15642ce47c4a5f48fa0a
gzahn <gzahn@uvu.edu>
2023-11-04 18:08:06 -0600

    add function for linking sample communities

---
+++ R/link_sample_communities.R
@@ -0,0 +1,31 @@
+
+# Force similarity between groups of samples in terms of taxa counts
+# n.links = number of groupings to force. Must be >= 1 and < sample.number/group.size
+# group.size = how many samples to make similar in each grouping. Must be >= 2 and <= sample.number/n.links
+# link.strength = how strong should the similarity function be. 0 = no change; 1=make identical.
+
+link_sample_communities <- function(dat, n.links = 1, group.size = 3, link.strength = .9){
+  sample.number = nrow(dat)
+  group.size <- round(group.size)
+  
+  stopifnot(n.links >= 1 & class(n.links) == "numeric" & n.links <= sample.number/group.size)
+  stopifnot(group.size >= 2 & class(group.size) == "numeric" & group.size <= sample.number/n.links)
+  stopifnot(link.strength <= 1 & link.strength >= 0 & class(link.strength) == "numeric")
+  
+  for(i in 1:n.links){
+  # pick random samples to link up
+  grouped.samples <- sample(row.names(dat),group.size)
+  first.sample <- grouped.samples[1]
+  other.samples <- grouped.samples[-1]
+  
+  # get difference values between first and other samples
+  # use link.strength to make them match first sample
+  for(i in seq_along(other.samples)){
+    sample.diff <- dat[first.sample,] - dat[other.samples[i],]
+    dat[other.samples[i],] <- dat[other.samples[i],] + round(sample.diff * link.strength)
+    }
+  message(paste0("Samples: ",paste(other.samples,collapse = ", ")," were made to resemble Sample: ",first.sample))
+  }
+  return(dat)
+}
+
