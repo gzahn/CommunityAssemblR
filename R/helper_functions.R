03113799b22e59845ff554f6310db2d4abbd4370
gzahn <gzahn@uvu.edu>
2023-11-06 19:18:07 -0700

    add basic stochastic transplant function

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -6,7 +6,6 @@ plot_hubs <- function(graph,bigpoint=10,littlepoint=3){
   plot(graph,
        layout = am.coord,
        vertex.size=(scale01(abs(igraph::authority_score(graph)$vector)) * 10)+3,
-         #ifelse(1:length(graph) %in% artpoints.z, bigpoint, littlepoint),
        vertex.label=NA)
 }
 
@@ -33,63 +32,63 @@ plot_hubs <- function(graph,bigpoint=10,littlepoint=3){
 # maybe make an if() that changes where to do the transformation?
 
 
-# function to gather up donor microbiome community relative abundances before and after transplanting
-check_transplant_success <- function(donor,transplant,newtaxa.only=FALSE,tidy=TRUE,keep.all.taxa=TRUE){
-
-
-  if(keep.all.taxa == TRUE){
-    donor_ra <- t(apply(donor,1,function(x){x/sum(x)}))
-    recipient_ra <- t(apply(transplant,1,function(x){x/sum(x)}))
-  }
-
-  if(keep.all.taxa == FALSE){
-    donor_ra <- t(apply(donor,1,function(x){x/sum(x)}))
-    recipient_ra <- t(apply(transplant[,colnames(donor_ra)],1,function(x){x/sum(x)}))
-  }
-
-
-
-  # if newtaxa.only=TRUE, subset to just the "newtaxa"
-  if(newtaxa.only == TRUE){
-    initial <- donor_ra[,grep(colnames(donor_ra),pattern="newtaxon")]
-    final <- recipient_ra[,colnames(initial)]
-  }
-
-  # in newtaxa.only=FALSE, then just subset to the taxa found in the donor community
-  if(newtaxa.only == FALSE){
-    initial <- donor_ra
-    final <- recipient_ra[,colnames(initial)]
-  }
-
-  # convert to data.frames
-  initial <- as.data.frame(initial)
-  final <- as.data.frame(final)
-
-  # add columns indicating timepoint
-  initial$timepoint <- "Initial"
-  final$timepoint <- "Final"
-
-  # join data frames
-  initial <- initial[,names(initial)[order(names(initial))]]
-  final <- final[,names(initial)[order(names(initial))]]
-  df <- rbind(initial,final)
-
-  df$sample <- rep(row.names(initial),2)
-
-  if(tidy){
-    df <- reshape(df,grep("tax",names(df),value = TRUE),direction = 'long',
-                  v.names = "realative_abundance",times = grep("tax",names(df),value = TRUE))
-    df$id <- NULL
-    row.names(df) <- NULL
-    names(df) <- c("timepoint","sample","taxon","relative_abundance")
-  }
-
-  return(df)
-}
+# # function to gather up donor microbiome community relative abundances before and after transplanting
+# check_transplant_success <- function(donor,transplant,newtaxa.only=FALSE,tidy=TRUE,keep.all.taxa=TRUE){
+#
+#
+#   if(keep.all.taxa == TRUE){
+#     donor_ra <- t(apply(donor,1,function(x){x/sum(x)}))
+#     recipient_ra <- t(apply(transplant,1,function(x){x/sum(x)}))
+#   }
+#
+#   if(keep.all.taxa == FALSE){
+#     donor_ra <- t(apply(donor,1,function(x){x/sum(x)}))
+#     recipient_ra <- t(apply(transplant[,colnames(donor_ra)],1,function(x){x/sum(x)}))
+#   }
+#
+#
+#
+#   # if newtaxa.only=TRUE, subset to just the "newtaxa"
+#   if(newtaxa.only == TRUE){
+#     initial <- donor_ra[,grep(colnames(donor_ra),pattern="newtaxon")]
+#     final <- recipient_ra[,colnames(initial)]
+#   }
+#
+#   # in newtaxa.only=FALSE, then just subset to the taxa found in the donor community
+#   if(newtaxa.only == FALSE){
+#     initial <- donor_ra
+#     final <- recipient_ra[,colnames(initial)]
+#   }
+#
+#   # convert to data.frames
+#   initial <- as.data.frame(initial)
+#   final <- as.data.frame(final)
+#
+#   # add columns indicating timepoint
+#   initial$timepoint <- "Initial"
+#   final$timepoint <- "Final"
+#
+#   # join data frames
+#   initial <- initial[,names(initial)[order(names(initial))]]
+#   final <- final[,names(initial)[order(names(initial))]]
+#   df <- rbind(initial,final)
+#
+#   df$sample <- rep(row.names(initial),2)
+#
+#   if(tidy){
+#     df <- reshape(df,grep("tax",names(df),value = TRUE),direction = 'long',
+#                   v.names = "realative_abundance",times = grep("tax",names(df),value = TRUE))
+#     df$id <- NULL
+#     row.names(df) <- NULL
+#     names(df) <- c("timepoint","sample","taxon","relative_abundance")
+#   }
+#
+#   return(df)
+# }
 
 #
 
-
+# library(tidyverse)
 # comm <- build_even_community(n.taxa = 30, n.samples = 10,taxa.sd = 300)
 # donor <- build_donor_community(comm,n.transplant.taxa = 10, overlap = .5)
 # transplant <- transplant_w_facilitation(comm,donor,facil.strength = 10,facil.ubiq = 1)
@@ -104,8 +103,8 @@ check_transplant_success <- function(donor,transplant,newtaxa.only=FALSE,tidy=TR
 #   geom_tile() +
 #   facet_wrap(~factor(timepoint,levels = c("Initial","Final"))) +
 #   theme(axis.text.x = element_blank())
+# #
 #
-
+# #
 #
-
-
+#
